7bec63aa0bd5447521d1a45710f01d033aa4b750
Erick Avendaño <117497394+erickavendanoh@users.noreply.github.com>
2024-10-04 01:08:37 -0600

    Creación de un primer modelo y sus respectivos migración, factory y seeder para crear registros

---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
+ */
+class UsuarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->name,
+            'email' => $this->faker->email,
+            'ciudad' => $this->faker->city,
+            'edad' => rand(10, 90)
+        ];
+    }
+}
